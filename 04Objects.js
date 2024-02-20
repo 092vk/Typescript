e037562c02e406d08b8ebeef72e57e8b78358811
092vk <vk092kumar@gmail.com>
2024-02-20 11:03:10 +0530

    a

---
+++ 04Objects.js
@@ -0,0 +1,9 @@
+"use strict";
+// bad behaviour of objects in typescript 
+Object.defineProperty(exports, "__esModule", { value: true });
+// making a typical object , this is same as js 
+var User = {
+    name: "vivek ray",
+    branch: "ece",
+    roll: "22bec092"
+};
